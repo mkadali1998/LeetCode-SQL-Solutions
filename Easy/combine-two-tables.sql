4cbb6311b117bcaad7bb68bf68bf08a9b66b7fbf
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-08-18 23:10:27 +0530

    Create combine-two-tables.sql

---
+++ Easy/combine-two-tables.sql
@@ -0,0 +1,6 @@
+-- Problem: 175. Combine Two Tables
+-- Link: https://leetcode.com/problems/combine-two-tables/description/
+
+SELECT firstName, lastName, city, state
+FROM Person 
+LEFT JOIN Address USING (personId)
