6503681ce178c582e1ffbe7e3552ce5a3b0b277d
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-08-19 16:22:56 +0530

    180

---
+++ Medium/consecutive-numbers.sql
@@ -0,0 +1,8 @@
+-- Problem: 180. Consecutive Numbers
+-- Link: https://leetcode.com/problems/consecutive-numbers/
+
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM logs l1
+JOIN logs l2 ON l1.id = l2.id - 1
+JOIN logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l2.num = l3.num
