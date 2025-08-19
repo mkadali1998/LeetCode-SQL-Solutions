dc8a70a3cd9f25f3f55d9fbda9737c6e3b182b4e
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-08-19 16:18:07 +0530

    176

---
+++ Medium/second-highest-salary.sql
@@ -0,0 +1,10 @@
+-- Problem: 176. Second Highest Salary
+-- Link: https://leetcode.com/problems/second-highest-salary/description/
+
+SELECT 
+    (
+        SELECT DISTINCT salary
+        FROM Employee
+        ORDER BY salary DESC
+        LIMIT 1 OFFSET 1
+    ) AS SecondHighestSalary;
