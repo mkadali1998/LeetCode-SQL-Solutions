bfda68c17e264629cad02e05186b13fe62b50313
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-08-22 13:54:37 +0530

    595

---
+++ Easy/big-countries.sql
@@ -0,0 +1,6 @@
+-- Problem: 595. Big Countries
+-- Link: https://leetcode.com/problems/big-countries/description/
+
+SELECT name, population, area
+FROM world
+WHERE area >= 3000000 OR population >= 25000000
