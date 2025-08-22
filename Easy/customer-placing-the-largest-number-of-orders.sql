b0c8c59c84754a6429cfb0e4f3cc0f31579a806d
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-08-22 13:53:20 +0530

    586

---
+++ Easy/customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,8 @@
+-- Problem: 586. Customer Placing the Largest Number of Orders
+-- Link: https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/description/
+
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
