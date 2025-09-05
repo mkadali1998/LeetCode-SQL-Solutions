190994dd7f66ae072ae147a1e7737ea352afec71
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-09-05 22:24:54 +0530

    610

---
+++ Easy/triangle-judgement.sql
@@ -0,0 +1,12 @@
+-- Problem: 610. Triangle Judgement
+-- Link: https://leetcode.com/problems/triangle-judgement/description/
+
+SELECT 
+    x, 
+    y, 
+    z, 
+    CASE 
+        WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM triangle;
