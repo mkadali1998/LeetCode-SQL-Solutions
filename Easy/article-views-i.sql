a1767cc713e1cfecd27155bf4b2b15716a79a8fc
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-09-05 23:57:01 +0530

    1148

---
+++ Easy/article-views-i.sql
@@ -0,0 +1,7 @@
+-- Problem: 1148. Article Views I
+-- Link: https://leetcode.com/problems/article-views-i/description/
+
+SELECT DISTINCT author_id AS id
+FROM views
+WHERE author_id = viewer_id
+ORDER BY author_id
