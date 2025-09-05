ea961880dcfa7c65e3849a8072058923ae021b43
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-09-05 23:55:34 +0530

    1141

---
+++ Easy/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,9 @@
+-- Problem: 1141. User Activity for the Past 30 Days I
+-- Link: https://leetcode.com/problems/user-activity-for-the-past-30-days-i/description/
+
+SELECT 
+  activity_date AS day,
+  COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN DATE_SUB('2019-07-27', INTERVAL 29 DAY) AND '2019-07-27'
+GROUP BY activity_date;
