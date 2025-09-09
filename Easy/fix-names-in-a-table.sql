e1e38b5dd5caca7305a0cadb7c3e721454344c8c
Monika Kadali <126834650+mkadali1998@users.noreply.github.com>
2025-09-09 15:52:12 +0530

    1667

---
+++ Easy/fix-names-in-a-table.sql
@@ -0,0 +1,8 @@
+-- Problem: 1667. Fix Names in a Table
+-- Link: https://leetcode.com/problems/fix-names-in-a-table/description/
+
+SELECT 
+    user_id,
+    CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2))) AS name
+FROM Users
+ORDER BY user_id
